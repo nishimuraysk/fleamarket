64f04b6f20e10447566c555f415f4a1fa308e56c
yusuke.nishimura <y.nishimura1992@gmail.com>
2024-01-11 16:42:39 +0900

    共通部分の調整

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Attendance Management</title>
+    <title>coachtechフリマ</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     @yield('css')
@@ -16,19 +16,22 @@
         <div class="header__inner">
             <div class="header-utilities">
                 <a class="header__logo" href="/">
-                    Attendance Management
+                    coachtechフリマ
                 </a>
                 <nav>
                     <ul class="header-nav">
                         @if (Auth::check())
                         <li class="header-nav__item">
-                            <a class="header-nav__link" href="/mypage">マイページ</a>
-                        </li>
-                        <li class="header-nav__item">
                             <form class="form" action="/logout" method="post">
                                 @csrf
                                 <button class="header-nav__button">ログアウト</button>
                             </form>
+                        </li>
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="/mypage">マイページ</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="/sell">出品</a>
                         </li>
                         @endif
                     </ul>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +15,5 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [AuthController::class, 'index']);
+    Route::get('/', [ItemController::class, 'index']);
 });
