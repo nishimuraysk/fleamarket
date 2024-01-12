3a5de84c273a4d5c80b6396af05d7c7983bbf0ec
yusuke.nishimura <y.nishimura1992@gmail.com>
2024-01-12 16:55:03 +0900

    共通ヘッダーのCSS調整

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -18,6 +18,17 @@
                 <a class="header__logo" href="/">
                     coachtechフリマ
                 </a>
+                <div class="search-box">
+                    <form action="/search" class="search-form" method="get">
+                        @csrf
+                        <input type="text" name="keyword" placeholder="なにをお探しですか？" class="search-text">
+                        <button class="search-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                            </svg>
+                        </button>
+                    </form>
+                </div>
                 <nav>
                     <ul class="header-nav">
                         @if (Auth::check())
@@ -30,8 +41,18 @@
                         <li class="header-nav__item">
                             <a class="header-nav__link" href="/mypage">マイページ</a>
                         </li>
+                        <li class="header-nav__item-sell">
+                            <a class="header-nav__link-sell" href="/sell">出品</a>
+                        </li>
+                        @else
                         <li class="header-nav__item">
-                            <a class="header-nav__link" href="/sell">出品</a>
+                            <a class="header-nav__link" href="/login">ログイン</a>
+                        </li>
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="/register">会員登録</a>
+                        </li>
+                        <li class="header-nav__item-sell">
+                            <a class="header-nav__link-sell" href="/sell">出品</a>
                         </li>
                         @endif
                     </ul>
